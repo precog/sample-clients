0efe7803cf07447e191854ae74a141eddf831a8e
Kris Nuttycombe <kris@hylotech.com>
2011-07-29 15:57:07 -0600

    Added string-based client example.

---
+++ java-client-sample/src/main/java/com/reportgrid/ReportGridStringSample.java
@@ -0,0 +1,30 @@
+package com.reportgrid;
+
+import com.reportgrid.api.*;
+import com.reportgrid.api.json.ToJson;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class ReportGridStringSample {
+		private static class TestData {
+			private final int testInt = 42;	
+			private final String testStr = "Hello World";
+		}
+
+    public static final ToJson<String> toJson = new ToJson<String>() {
+      public String serialize(String string) {
+        return string;
+      }
+    };
+
+    public static void main(String[] argv) {
+      try {
+        TrackingClient testClient = new TrackingClient(Service.ProductionHttp, TrackingClient.TEST_TOKEN);
+        Event<String> testEvent = new Event<String>(new Date(), "test", "{\"testInt\":42, \"testStr\":\"Hello World\"}", 1);
+        testClient.track(new Path("/test"), testEvent, false, toJson);
+      } catch (IOException ex) {
+        ex.printStackTrace(); 
+      }
+    }
+}
